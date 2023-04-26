d5fe64a8f6eee4ebf5a616d316f9d9b0311d0534
Aleksandr Trufmanov <kride2009@yandex.ru>
2023-04-26 19:55:29 +0300

    Code changed

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,6 +1,6 @@
 public class BmiService {
-    public int calculate(int weight, int height) {
-    double bmi = weight / (height * height);
-    return bmi;
+    public int calculate(double height, int weight) {
+        int bmi = (int) (weight / height / height);
+        return bmi;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,10 @@
+
 public class Main {
     public static void main(String[] args) {
-       BmiService service = new BmiService();
+        BmiService service = new BmiService();
         double height = 1.87;
         int weight = 98;
-       double bmi = service.calculate(height, weight);
+        int bmi = service.calculate(height, weight);
         System.out.println(bmi);
     }
 }
